908bb0074c767c047a740c67e956931407ec73f4
Angelo <angelooparah@gmail.com>
2018-01-06 22:40:39 +0000

    minor

--- ui/src/main/scala/components/CardImage.scala
+++ ui/src/main/scala/components/CardImage.scala
@@ -8,6 +8,8 @@ case class CardImageBuilder() extends ComponentBuilder {
     def render = this
     var url: String = _    
     var alt: String = _   
+    
+    //TODO make size variable   
     @dom def build = <figure class="image is-48x48">
       								<img src={url} alt={alt}/>
     								 </figure>.asInstanceOf[HTMLElement]
