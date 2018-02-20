01e5705e6399f785e43ee690c479cada9cb1787c
Angelo <angelooparah@gmail.com>
2018-02-20 21:53:42 +0000

    ability to retrive user from authenticated requests, got rid of warning by adding option to list of allowed methods

--- authentication-server/app/controllers/ApplicationController.scala
+++ authentication-server/app/controllers/ApplicationController.scala
@@ -11,36 +11,33 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import repos.UserRepository
 import models.RegisteredUser
 import play.api.Logger
+import reactivemongo.bson.BSONObjectID
 
-
+// overkill
 sealed trait Gender
 case object Male extends Gender
 case object Female extends Gender
+case object Gender extends Gender{
+  import scala.language.implicitConversions
+  import JsonFormats.GenderFormat.writes
+  implicit def asString(gender: Gender): String = writes(gender).as[String]
+}
 
 case object JsonFormats{
   implicit val loginFormat: OFormat[LoginDetails] = Json.format[LoginDetails]
-//  implicit object GenderReads extends Reads[Gender] {
-//    override def reads(json: JsValue): JsResult[Gender] = json.as[String].toLowerCase() match{
-//      case "male" => JsSuccess(Male)
-//      case "female" => JsSuccess(Female)
-//      case _ => JsError("Invalid Gender")
-//    }
-//  }
-  implicit object GenderFormat extends OFormat[Gender] {
+  implicit val registrationFormat: OFormat[RegistrationDetails] = Json.format[RegistrationDetails]
+  implicit object GenderFormat extends Format[Gender] {
     def reads(json: JsValue): JsResult[Gender] = json.as[String].toLowerCase() match{
       case "male" => JsSuccess(Male)
       case "female" => JsSuccess(Female)
       case _ => JsError("Invalid Gender")
-    }
-    
+    } 
     def writes(gender: Gender) = gender match {
-      case Male => JsObject(Seq(("gender", Json.toJson("male"))))
-      case Female => JsObject(Seq(("gender", Json.toJson("female"))))
-    }
+      case Male => JsString("male")
+      case Female => JsString("female")
+    }   
   }
-  implicit val registrationFormat: OFormat[RegistrationDetails] = Json.format[RegistrationDetails]
 }
-
 
 case class LoginDetails(username: String, password: String)
 case class RegistrationDetails(name: String, username: String, email: String, password: String, gender: Gender)
@@ -53,13 +50,12 @@ extends SecuredController(scc){
 //      (JsPath \ "username").read[String] and
 //      (JsPath \ "password").read[String]).tupled
     
-  //TODO create a class rather than using tuples
-  val registrationDetails: Reads[(String, String, String, String, Gender)] = (
-      (JsPath \ "name").read[String] and
-      (JsPath \ "username").read[String] and
-      (JsPath \ "email").read[String] and
-      (JsPath \ "password").read[String] and
-      (JsPath \ "gender").read[Gender]).tupled //TODO use sealed trait
+//  val registrationDetails: Reads[(String, String, String, String, Gender)] = (
+//      (JsPath \ "name").read[String] and
+//      (JsPath \ "username").read[String] and
+//      (JsPath \ "email").read[String] and
+//      (JsPath \ "password").read[String] and
+//      (JsPath \ "gender").read[Gender]).tupled
    
   
   def login = Action(parse.json).async { implicit request: Request[JsValue] =>
@@ -87,15 +83,44 @@ extends SecuredController(scc){
     val result = request.body
       .validate[RegistrationDetails]
       .map({ case RegistrationDetails(name, username, email, password, gender) => { //TODO check if user already exists...
-                //Logger.info(GenderFormat.writes(gender).toString)
-                val registeredUser = RegisteredUser(name, username, email, password, gender.toString.toLowerCase)
+                //Logger.info(GenderFormat.asString(gender))
+                val registeredUser = RegisteredUser(name, username, email, password, gender)
                 repository.addUser(registeredUser) 
-                val apiUser = User(name, Some(registeredUser._id.stringify))
+                val apiUser = User(name, Some(registeredUser._id.stringify)) //TODO made api user id not optional, remove underscore
                 Ok.addingToJwtSession("user", write(apiUser))
              }
            })
       .recover({ case ex => BadRequest(JsError.toJson(ex.errors)) })
       
     Future{result.get}
+  }
+  
+  def getAll = AuthenticatedAction.async { 
+    repository.getAll.map(all => Ok(write(all.map(x => User(x.name, Some(x._id.stringify)))))) 
+  }
+  
+//  def postUser = Action.async(parse.json) { req =>   
+//     val payload: String = Json.stringify(req.body)
+//     val user = read[User](payload)
+//     repository.addUser(user).map(_ => Created)
+//  }
+
+  def deleteUser = AuthenticatedAction.async(parse.json) { req => 
+     //val id: String = req.body.as[String]
+     val id = req.user._id.get //TODO this id belongns to the logged in user...self deletion, remove parse json if not used
+     repository.deleteUser(BSONObjectID.parse(id).get).map({
+       case Some(user) => Ok(write(User(user.name, Some(user._id.stringify))))
+       case None => NotFound
+     })
+  }
+
+  def updateUser = AuthenticatedAction.async(parse.json) { req => 
+      //val payload: String = Json.stringify(req.body)
+      val id = req.user._id.get
+      val updated = req.body.validate[RegisteredUser].get //read[User](payload)
+      repository.updateUser(BSONObjectID.parse(id).get, updated).map({
+        case Some(user) => Ok(write(User(user.name, Some(user._id.stringify))))
+        case None => NotFound
+      })
   }
 }
--- authentication-server/app/controllers/SecuredController.scala
+++ authentication-server/app/controllers/SecuredController.scala
@@ -17,25 +17,40 @@ import play.api.libs.json.Json
 
 class AuthenticatedRequest[A](val user: User, request: Request[A]) extends WrappedRequest[A](request)
 
-protected class AuthenticatedActionBuilder @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext)
-    extends ActionBuilderImpl(parser) {
-  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
-    val n = request.jwtSession + ("user", write(User("Hello", Some("5a8a9024505c125468cd4121")))) //("user", """{'name': 'Paul', '_id': '5a8a9024505c125468cd4121'}""")
-    println("EEEEE", n.get("user"))
-    n.get("user") match {
+protected class AuthenticatedActionBuilder @Inject()(bodyParser: BodyParsers.Default)(implicit ec: ExecutionContext)
+    extends ActionBuilder[AuthenticatedRequest, AnyContent] {
+  
+  override def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]): Future[Result] = {
+     request.jwtSession.get("user") match {
       case Some(json) => {
-        Logger.info("Parsing user")
-        Logger.info(Json.stringify(json))
-        Logger.info(json.as[String])
+//        Logger.info("Parsing user")
+//        Logger.info(Json.stringify(json))
+//        Logger.info(json.as[String])
         val user = read[User](json.as[String])  
         block(new AuthenticatedRequest[A](user, request)).map(_.refreshJwtSession(request))
       }     
-      case _ =>
-        Future(Unauthorized)
+      case _ => Future(Unauthorized)
     }
   }
+  def parser: BodyParser[AnyContent] = bodyParser
+  protected def executionContext = ec
 }
 
+case class SecuredControllerComponents @Inject()(
+    //adminActionBuilder: AdminActionBuilder,
+    authenticatedActionBuilder: AuthenticatedActionBuilder,
+    actionBuilder: DefaultActionBuilder,
+    parsers: PlayBodyParsers,
+    messagesApi: MessagesApi,
+    langs: Langs,
+    fileMimeTypes: FileMimeTypes,
+    executionContext: scala.concurrent.ExecutionContext
+) extends ControllerComponents
+
+class SecuredController @Inject()(scc: SecuredControllerComponents) extends AbstractController(scc) {
+  //def AdminAction: AdminActionBuilder                 = scc.adminActionBuilder
+  def AuthenticatedAction: AuthenticatedActionBuilder = scc.authenticatedActionBuilder
+}
 
 //
 //case class Logging[A](action: Action[A]) extends Action[A] {
@@ -77,18 +92,21 @@ protected class AuthenticatedActionBuilder @Inject()(parser: BodyParsers.Default
 //  }
 //}
 
-case class SecuredControllerComponents @Inject()(
-    //adminActionBuilder: AdminActionBuilder,
-    authenticatedActionBuilder: AuthenticatedActionBuilder,
-    actionBuilder: DefaultActionBuilder,
-    parsers: PlayBodyParsers,
-    messagesApi: MessagesApi,
-    langs: Langs,
-    fileMimeTypes: FileMimeTypes,
-    executionContext: scala.concurrent.ExecutionContext
-) extends ControllerComponents
-
-class SecuredController @Inject()(scc: SecuredControllerComponents) extends AbstractController(scc) {
-  //def AdminAction: AdminActionBuilder                 = scc.adminActionBuilder
-  def AuthenticatedAction: AuthenticatedActionBuilder = scc.authenticatedActionBuilder
-}
+//protected class AuthenticatedActionBuilder @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext)
+//    extends ActionBuilderImpl(parser) {
+//  override def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]): Future[Result] = {
+//    //val n = request.jwtSession + ("user", write(User("Hello", Some("5a8a9024505c125468cd4121")))) //("user", """{'name': 'Paul', '_id': '5a8a9024505c125468cd4121'}""")
+//    //println("EEEEE", n.get("user"))
+//     request.jwtSession.get("user") match {
+//      case Some(json) => {
+//        Logger.info("Parsing user")
+//        Logger.info(Json.stringify(json))
+//        Logger.info(json.as[String])
+//        val user = read[User](json.as[String])  
+//        block(new AuthenticatedRequest[A](user, request)).map(_.refreshJwtSession(request))
+//      }     
+//      case _ =>
+//        Future(Unauthorized)
+//    }
+//  }
+//}
