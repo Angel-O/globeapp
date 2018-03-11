f121ac60bc0bc250ca4f4ae7124818245ee47b7c
Angelo <angelooparah@gmail.com>
2018-03-11 11:20:29 +0000

    formatting code

--- app-server/app/controllers/MobileAppController.scala
+++ app-server/app/controllers/MobileAppController.scala
@@ -23,44 +23,55 @@ class MobileAppController @Inject() (
   def getApp(id: String) = Action.async {
     Logger.info("Fetching mobile app")
     parseId(id)
-      .flatMap(validId => repository.getApp(validId).map({
-        case Some(mobileApp) => Ok(write(mobileApp))
-        case None            => NotFound
-      }))
+      .flatMap(validId =>
+        repository
+          .getApp(validId)
+          .map({
+            case Some(mobileApp) => Ok(write(mobileApp))
+            case None            => NotFound
+          }))
       .recover({ case ex => Logger.error(ex.getMessage); BadRequest })
   }
 
   def postApp = AuthenticatedAction.async(parse.json) { req =>
     Logger.info("Creating mobile app")
-    req.body.validate[MobileApp].map(app =>
-      parseId(app._id).flatMap(validId =>
-        repository.addApp(app)
-          .map(_ => Created(validId))
-          .recover({ case ex => Logger.error(ex.getMessage); BadRequest })))
+    req.body.validate[MobileApp]
+      .map(app =>
+        parseId(app._id)
+          .flatMap(validId =>
+            repository
+              .addApp(app)
+              .map(_ => Created(validId))
+              .recover({ case ex => Logger.error(ex.getMessage); BadRequest })))
       .getOrElse({ Logger.error("Invalid payload"); Future(BadRequest) })
   }
 
   def deleteApp(id: String) = AuthenticatedAction.async(parse.json) { req =>
     Logger.info("Deleting mobile app")
-    parseId(id).flatMap(validId => repository
-      .deleteApp(validId)
-      .map({
-        case Some(mobileApp) => Ok(write(mobileApp))
-        case None            => NotFound
-      }))
+    parseId(id)
+      .flatMap(validId =>
+        repository
+          .deleteApp(validId)
+          .map({
+            case Some(mobileApp) => Ok(write(mobileApp))
+            case None            => NotFound
+          }))
       .recover({ case ex => Logger.error(ex.getMessage); BadRequest })
   }
 
   def updateApp(id: String) = AuthenticatedAction.async(parse.json) { req =>
     Logger.info("Updating mobile app")
-    req.body.validate[MobileApp].map(app =>
-      parseId(id).flatMap(validId => repository
-        .updateApp(validId, app)
-        .map({
-          case Some(mobileApp) => Ok(write(mobileApp))
-          case None            => NotFound
-        }))
-        .recover({ case ex => Logger.error(ex.getMessage); BadRequest }))
+    req.body.validate[MobileApp]
+      .map(app =>
+        parseId(id)
+          .flatMap(validId =>
+            repository
+              .updateApp(validId, app)
+              .map({
+                case Some(mobileApp) => Ok(write(mobileApp))
+                case None            => NotFound
+              }))
+          .recover({ case ex => Logger.error(ex.getMessage); BadRequest }))
       .getOrElse({ Logger.error("Invalid payload"); Future(BadRequest) })
   }
 
