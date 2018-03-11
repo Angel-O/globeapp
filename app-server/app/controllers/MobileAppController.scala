717b53991da378570189db32bc56321ba55c1ddf
Angelo <angelooparah@gmail.com>
2018-03-11 00:52:16 +0000

    consolidated app-moodel and app-server

--- app-server/app/controllers/MobileAppController.scala
+++ app-server/app/controllers/MobileAppController.scala
@@ -4,62 +4,53 @@ import javax.inject.Inject
 import play.api.Logger
 import repos.MobileAppRepository
 import play.api.libs.json.Json
-import apimodels.{MobileApp => ApiApp}
+import apimodels.mobileapp.MobileApp
 import upickle.default._
 import scala.concurrent.ExecutionContext.Implicits.global
-import models.MobileApp, models.ConversionHelpers._
-import reactivemongo.bson.BSONObjectID 
+import reactivemongo.bson.BSONObjectID
 
-class MobileAppController @Inject()(scc: SecuredControllerComponents,
-                                    repository: MobileAppRepository)
-    extends SecuredController(scc) {
+class MobileAppController @Inject() (
+  scc:        SecuredControllerComponents,
+  repository: MobileAppRepository)
+  extends SecuredController(scc) {
 
-  def getAll = AuthenticatedAction.async {
+  def getAll = Action.async {
     Logger.info("Fetching mobile apps")
-    repository.getAll.map(all => Ok(write(all.map(_.toApi))))
+    repository.getAll.map(all => Ok(write(all)))
   }
-  
-  // url param
-  def getApp(id: String) = AuthenticatedAction.async {
+
+  def getApp(id: String) = Action.async {
     Logger.info("Fetching mobile app")
-    
-//    BSONObjectID.parse(id).map(validId => ...)
-//    .recover({case ex => BadRequest})
-    
-    repository.getApp(BSONObjectID.parse(id).get).map({
-      case Some(mobileApp) => Ok(write(mobileApp.toApi))
-      case None => NotFound
+    repository.getApp(id).map({
+      case Some(mobileApp) => Ok(write(mobileApp))
+      case None            => NotFound
     })
   }
 
-  def postApp = Action.async(parse.json) { req =>
+  def postApp = AuthenticatedAction.async(parse.json) { req =>
+    Logger.info("Creating mobile app")
     val payload: String = Json.stringify(req.body)
-    val apiApp = read[ApiApp](payload)
-    repository.addApp(apiApp.toModel).map(_ => Created)
+    val app = read[MobileApp](payload)
+    repository.addApp(app).map(_ => Created)
   }
 
-  //TODO fix this
   def deleteApp(id: String) = AuthenticatedAction.async(parse.json) { req =>
     Logger.info("Deleting mobile app")
-    val id: String = req.body.as[String]
     repository
-      .deleteApp(BSONObjectID.parse(id).get)
+      .deleteApp(id)
       .map({
-        case Some(mobileApp) => Ok(write(mobileApp.toApi))
+        case Some(mobileApp) => Ok(write(mobileApp))
         case None            => NotFound
       })
   }
 
-  //TODO fix this
   def updateApp(id: String) = AuthenticatedAction.async(parse.json) { req =>
     Logger.info("Updating mobile app")
-    //val payload: String = Json.stringify(req.body)
-    val id = req.user.id
-    val updated = req.body.validate[MobileApp].get //read[User](payload)
+    val updated = req.body.validate[MobileApp].get
     repository
-      .updateApp(BSONObjectID.parse(id).get, updated)
+      .updateApp(id, updated)
       .map({
-        case Some(mobileApp) => Ok(write(mobileApp.toApi))
+        case Some(mobileApp) => Ok(write(mobileApp))
         case None            => NotFound
       })
   }
